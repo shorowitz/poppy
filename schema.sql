99e1547beb9ee6ac982ecad02a2dd8fb96bae064
Sara Horowitz <sslhorowitz@gmail.com>
2017-03-07 20:55:04 -0500

    initial commit - readme, schema and seeds

---
+++ schema.sql
@@ -0,0 +1,31 @@
+DROP TABLE if exists plants CASCADE;
+DROP TABLE if exists light_type CASCADE;
+DROP TABLE if exists light_options CASCADE;
+DROP TABLE if exists water CASCADE;
+
+CREATE TABLE plants (
+  id SERIAL PRIMARY KEY UNIQUE,
+  botanical_name TEXT,
+  english_name TEXT,
+  care_link TEXT,
+  image_url TEXT,
+  light_id INTEGER REFERENCES light_type,
+  water_id INTEGER REFERENCES water
+);
+
+CREATE TABLE light_options (
+  id SERIAL PRIMARY KEY UNIQUE,
+  description TEXT,
+  type_id INTEGER REFERENCES light_type
+);
+
+CREATE TABLE light_type (
+  id SERIAL PRIMARY KEY UNIQUE,
+  type TEXT
+);
+
+CREATE TABLE water (
+  id SERIAL PRIMARY KEY UNIQUE,
+  instruction TEXT,
+  frequency TEXT
+);

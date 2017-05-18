983ff167c5a3615f98631810a45e2bf6ac8d5602
carrie <cstreed@jibe.com>
2017-05-18 10:19:34 -0400

    adjusted text/photo positions for desktop

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -4,21 +4,50 @@ class Hero extends React.Component {
 
   render() {
     const style = {
-      background: 'url("../assets/hero-img.jpg")',
-      backgroundSize: 'cover',
-      backgroundPosition: '50% 50%',
-      height: '100vh'
+      main: {
+        position: 'relative',
+        background: 'url("../assets/hero-img.jpg")',
+        backgroundSize: 'cover',
+        backgroundPosition: '50% 90%',
+        height: '100vh'
+      },
+      headerText: {
+        color: '#243d46',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+        border: '1px solid blue',
+        height: '100vh',
+        paddingRight: 160,
+        h1: {
+          fontSize: 100,
+          color: '#fff'
+        }
+      },
+      arrow: {
+        height: 50,
+        width: '100%',
+        backgroundColor: '#fff',
+        position: 'absolute',
+        bottom: 0
+      }
     }
 
     return (
       <div
         className='hero-container'
-        style={style}>
-        <div>
-          <h1>OKCactus</h1>
-          <h2>Lorem Ipsum</h2>
+        style={style.main}>
+        <div
+          style={style.headerText}>
+            <h1
+              style={style.headerText.h1}>
+                OKCactus.
+            </h1>
+            <h2>lorem ipsum dolor, sit amet</h2>
         </div>
-        <div className='arrow-box'>
+        <div
+          style={style.arrow}>
         </div>
       </div>
     )

1af633b1063afd8da9268a1bcd0e3620ece95b36
Sara Horowitz <sslhorowitz@gmail.com>
2017-05-18 17:33:17 -0400

    results rendering correctly

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,62 +1,89 @@
 const React = require('react');
+const ReactDOM = require('react-dom');
+const ReactRouter = require('react-router-dom');
+const Router = ReactRouter.BrowserRouter;
+const Route = ReactRouter.Route;
 const $ = require('jquery');
 const LightOptions = require('./LightOptions');
 const WaterOptions = require('./WaterOptions');
+const ResultGrid = require('./ResultGrid');
 
 class Search extends React.Component {
 
-constructor(props) {
-  super(props)
-  this.state = {
-    lightid: -1,
-    waterid: -1
-  }
-  this.handleLightChange = this.handleLightChange.bind(this);
-  this.handleWaterChange = this.handleWaterChange.bind(this);
-  this.handleSubmit = this.handleSubmit.bind(this);
-}
-
-handleLightChange(value) {
-  this.setState({lightid : value})
-}
-
-handleWaterChange(value) {
-  this.setState({waterid : value})
-}
-
-handleSubmit(event) {
-  event.preventDefault();
-  this.searchDB()
-}
-
-searchDB () {
-  $.ajax({
-    url: '/search',
-    type: 'POST',
-    data: {
-      lightid: this.state.lightid,
-      waterid: this.state.waterid
+  constructor(props) {
+    super(props)
+    this.state = {
+      lightid: -1,
+      waterid: -1,
+      results: null
     }
-  }).done((data) =>{
-    console.log(data)
-  })
-}
+    this.handleLightChange = this.handleLightChange.bind(this);
+    this.handleWaterChange = this.handleWaterChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+
+  handleLightChange(value) {
+    this.setState({lightid : value})
+  }
+
+  handleWaterChange(value) {
+    this.setState({waterid : value})
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.refs.searchForm.reset();
+    this.searchDB()
+  }
+
+  handleReset() {
+    this.setState({results: null,
+                  lightid: -1,
+                  waterid: -1})
+  }
+
+  searchDB () {
+    if (this.state.lightid != -1 && this.state.waterid != -1) {
+      $.ajax({
+        url: '/search',
+        type: 'POST',
+        data: {
+          lightid: this.state.lightid,
+          waterid: this.state.waterid
+        }
+      }).done((data) =>{
+        console.log(data)
+        this.setState({results:data})
+      })
+    } else {
+      window.alert("make both selections please!")
+    }
+  }
 
   render() {
     return (
-      <div className='search-container'>
-        <form ref="searchForm" onSubmit={this.handleSubmit}>
-          <LightOptions
-            onSubmit={this.handleLightChange} />
-          <WaterOptions
-            onSubmit={this.handleWaterChange} />
-          <div>
-            <input type="submit" value="Search" />
+      <div>
+        {this.state.results === null ? (
+          <div className='search-container'>
+            <form ref="searchForm" onSubmit={this.handleSubmit}>
+              <LightOptions
+                onSubmit={this.handleLightChange} />
+              <WaterOptions
+                onSubmit={this.handleWaterChange} />
+              <div>
+                <input type="submit" value="Search" />
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
+        ):(
+          <ResultGrid results={this.state.results} onClick={this.handleReset}/>
+        )}
+    </div>
+
     )
   }
+
 }
 
 module.exports = Search;

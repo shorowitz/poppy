08916d05a6c64a772cbc5313aca82f0650d520ec
carrie <cstreed@jibe.com>
2017-05-26 00:48:24 -0400

    initial form styles for light-conditions

--- app/components/LightOptions.js
+++ app/components/LightOptions.js
@@ -25,23 +25,58 @@ class LightOptions extends React.Component {
   }
 
   render() {
+    const style = {
+      main: {
+        display: 'flex'
+      },
+      form: {
+        width: '50%',
+        background: '#faf9f9',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        textAlign: 'right',
+        padding: '0 6%',
+        h3: {
+          color: '#b0899b'
+        },
+        select: {
+          border: '2px solid rgba(176, 137, 155, .4)'
+        }
+      },
+      image: {
+        height: '50vh',
+        width: '50%',
+        background: 'url("../assets/search-image1.jpg")',
+        backgroundSize: 'cover',
+        padding: '0 6%'
+      }
+    }
+
     return (
-      <div>
-        LIGHT CONDITIONS
-        <div>
-          <label>
-            Where in your apartment would you like your plant to live?
-            <select onChange={this.handleChange}>
-              <option value="0">Select a Window</option>
-              <option value="1">A room without windows / artifical light room</option>
-              <option value="2">North-facing window, 0-2 hours of sun</option>
-              <option value="3">East-facing window, 0-2 hours of sun</option>
-              <option value="4">West-facing window, 2+ hours of sun</option>
-              <option value="5">South-facing window, 4+ hours of sun</option>
-              <option value="6">Behind a glass block or sheer curtain on a North- or East-facing window</option>
-              <option value="7">Behind a glass block or sheer curtain on a South- or West-facing window</option>
-            </select>
-          </label>
+      <div style={style.main}>
+        <div style={style.form} className="search-form">
+
+          <h3 style={style.form.h3}> Light <br /> Conditions </h3>
+
+          <div>
+            <label>
+              <select style={style.form.select} onChange={this.handleChange}>
+                <option value="0">Where are your windows?</option>
+                <option value="1">A room without windows / artifical light room</option>
+                <option value="2">North-facing window, 0-2 hours of sun</option>
+                <option value="3">East-facing window, 0-2 hours of sun</option>
+                <option value="4">West-facing window, 2+ hours of sun</option>
+                <option value="5">South-facing window, 4+ hours of sun</option>
+                <option value="6">Behind a glass block or sheer curtain on a North- or East-facing window</option>
+                <option value="7">Behind a glass block or sheer curtain on a South- or West-facing window</option>
+              </select>
+            </label>
+          </div>
+        </div>
+
+        <div style={style.image}>
         </div>
       </div>
     )

68096e7bfc58fecb43f9686a26299edebf60335f
carrie <cstreed@jibe.com>
2017-07-11 22:36:11 -0400

    separated structure of forms from component and placed in css file

--- app/components/WaterOptions.js
+++ app/components/WaterOptions.js
@@ -23,20 +23,45 @@ class WaterOptions extends React.Component {
   }
 
   render() {
+    const style = {
+      main: {
+        flexDirection: 'row-reverse'
+      },
+      form: {
+        background: '#fff',
+        h3: {
+          color: '#243d46'
+        },
+        select: {
+          border: '2px solid rgba(36, 61, 70, .4)'
+        }
+      },
+      image: {
+        background: 'url("../assets/search-image2.jpg")',
+        backgroundSize: 'cover'
+      }
+    }
+
     return (
-      <div>
-        WATER FREQUENCY
-        <div>
-          <label>
-            How often can you check on your plant?
-            <select onChange={this.handleChange}>
-              <option value="0">Select Water Frequency</option>
-              <option value="1">Two or three times a week - Keep the soil lightly moist</option>
-              <option value="2">Once or twice a week - Let top inch of soil dry</option>
-              <option value="3">Every one or two weeks - Let the soil dry completely</option>
-              <option value="4">I Don't Care!</option>
-            </select>
-          </label>
+      <div className="form-container" style={style.main}>
+        <div className="search-form" style={style.form}>
+
+          <h3 style={style.form.h3}> Water <br /> Frequency </h3>
+
+          <div>
+            <label>
+              <select onChange={this.handleChange}>
+                <option value="0">how often can you water?</option>
+                <option value="1">Two or three times a week - Keep the soil lightly moist</option>
+                <option value="2">Once or twice a week - Let top inch of soil dry</option>
+                <option value="3">Every one or two weeks - Let the soil dry completely</option>
+                <option value="4">Don't Care!</option>
+              </select>
+            </label>
+          </div>
+        </div>
+
+        <div className="form-bg" style={style.image}>
         </div>
       </div>
     )

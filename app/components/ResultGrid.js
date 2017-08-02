62b0144778d73db214aad58f4e7aaaac5e006833
Sara Horowitz <sslhorowitz@gmail.com>
2017-08-02 14:59:22 -0400

    figured out mapping object and binding this for more-info

--- app/components/ResultGrid.js
+++ app/components/ResultGrid.js
@@ -1,14 +1,16 @@
 const React = require('react');
+const ReactDOM = require('react-dom');
 const $ = require('jquery');
-const Insta = require('./Insta');
 
 
 class ResultGrid extends React.Component {
 
   constructor(props){
     super(props)
+    this.state = { active: [] }
 
     this.handleClick = this.handleClick.bind(this)
+    this.toggleState = this.toggleState.bind(this)
   }
 
   handleClick(event) {
@@ -16,8 +18,13 @@ class ResultGrid extends React.Component {
     this.props.onClick();
   }
 
-  render () {
+  toggleState(index) {
+    let active = this.state.active.slice();
+    active[index] = !active[index];
+    this.setState({active});
+  }
 
+  _renderList() {
     const getImage = function (img){
       return {
         background: 'url('+ img + ')',
@@ -26,27 +33,32 @@ class ResultGrid extends React.Component {
       }
     }
 
+    return this.props.results.map( (result, index) => {
+              return (
+                <div onClick={() => this.toggleState(index)} key={result.botanical_name}>
+                  <div id={result.english_name} style={getImage(result.image_url)}></div>
+                  <div>
+                    <h4>{ result.english_name }</h4>
+                    <h5>{ result.botanical_name }</h5>
+                    <p>{ result.frequency }</p>
+                    <p>{ result.instruction }</p>
+                    <div>{ this.state.active[index] ? (<h1> ya </h1> ):( <h2> no </h2>) }</div>
+                  </div>
+                </div>
+              )
+            })
+  }
+
+  render () {
+
     return (
       <div>
-      <button onClick={this.handleClick}>Search Again</button>
-      <div>{this.props.results.length} Results</div>
-      <div> Window: {this.props.results[0].description} - {this.props.results[0].type}</div>
-      <div className='result-list'>
-        {this.props.results.map(function (result, index) {
-          return (
-            <div key={result.botanical_name}>
-              <div id={result.english_name} style={getImage(result.image_url)}></div>
-              <div>
-                <h4>{ result.english_name }</h4>
-                <h5>{ result.botanical_name }</h5>
-                <p>{ result.frequency }</p>
-                <p>{ result.instruction }</p>
-              </div>
-              <Insta plant={result.english_name} index={result.botanical_name}/>
-            </div>
-            )
-          })}
-      </div>
+        <button onClick={this.handleClick}>Search Again</button>
+        <div>{this.props.results.length} Results</div>
+        <div > Window: {this.props.results[0].description} - {this.props.results[0].type}</div>
+        <div className='result-list'>
+          {this._renderList()}
+        </div>
       </div>
     )
   }

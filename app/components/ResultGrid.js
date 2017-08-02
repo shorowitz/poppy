07310070b59642cc01ae4ef0f01f187e748f9b95
carrie <cstreed@jibe.com>
2017-08-01 20:50:41 -0400

    added styles for each result component

--- app/components/ResultGrid.js
+++ app/components/ResultGrid.js
@@ -17,12 +17,39 @@ class ResultGrid extends React.Component {
   }
 
   render () {
+    const style = {
+      resultsList: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center'
+      },
+      result: {
+        position: 'relative',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '40%',
+        margin: '25px 0'
+
+        textBox: {
+          position: 'absolute',
+          border: '2px solid #000',
+          padding: '30px 60px',
+          left: 160,
+          width: 140,
+          zIndex: '-1'
+        }
+      }
+    }
 
     const getImage = function (img){
       return {
         background: 'url('+ img + ')',
-        width: 50,
-        height: 50
+        width: 200,
+        height: 200,
+        border: '1px solid #ececec',
+        borderRadius: '50%',
+        backgroundSize: 'cover'
       }
     }
 
@@ -31,18 +58,17 @@ class ResultGrid extends React.Component {
       <button onClick={this.handleClick}>Search Again</button>
       <div>{this.props.results.length} Results</div>
       <div> Window: {this.props.results[0].description} - {this.props.results[0].type}</div>
-      <div className='result-list'>
+      <div style={style.resultsList} className='result-list'>
         {this.props.results.map(function (result, index) {
           return (
-            <div key={result.botanical_name}>
+            <div style={style.result} key={result.botanical_name}>
               <div id={result.english_name} style={getImage(result.image_url)}></div>
-              <div>
+              <div style={style.result.textBox}>
                 <h4>{ result.english_name }</h4>
                 <h5>{ result.botanical_name }</h5>
                 <p>{ result.frequency }</p>
                 <p>{ result.instruction }</p>
               </div>
-              <Insta plant={result.english_name} index={result.botanical_name}/>
             </div>
             )
           })}
